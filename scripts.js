94869328510fffca095c7f607712b1224b087e60
Mark <ratrateroo@gmail.com>
2020-02-27 20:50:26 +0800

    added logic for the button

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
- 
-window.addEventListener('scroll', () => {
-    const header = document.querySelector('.header');
-    header.classList.toggle('sticky', window.scrollY > 0);
-     
-    
-})
+window.addEventListener("scroll", () => {
+  const header = document.querySelector(".header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
 
+const mobileButton = document.querySelector(".mobile-button");
+mobileButton.addEventListener("click", () => {
+  mobileButton.classList.toggle("open");
+});
